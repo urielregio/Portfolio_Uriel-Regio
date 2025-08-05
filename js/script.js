57710c3f93de1c887b60f2dbc027dc8f1200894d
Uriel Régio <victorurielregio@gmail.com>
2025-08-05 11:23:03 -0300

    Atualiza o estilo e a estrutura do menu de navegação e da página inicial para melhorar a responsividade e a usabilidade.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+// ---- MENU LATERAL ----
+const btnMenu = document.getElementById('btn-menu');
+const btnFechar = document.getElementById('btn-fechar')
+const menu = document.getElementById('menu');
+const navLinks = document.querySelectorAll('#menu a')
 
+// ---- ABRIR MENU ----
+btnMenu.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+
+// ---- FECHAR MENU PELO BOTAO ----
+btnFechar.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+// ---- FECHAR MENU AO CLICAR EM QUALQUER LINK ----
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+});
+
+// ---- FECHAR MENU AO CLICAR FORA DELE ----
+document.addEventListener('click', (event) => {
+  const isClickInsideMenu = menu.contains(event.target);
+  const isClickOnBtnMenu = btnMenu.contains(event.target);
+
+  if (!isClickInsideMenu && !isClickOnBtnMenu) {
+    menu.classList.remove('active');
+  }
+});
